48486ffa9afc29cb7efc182b128e116f002c8004
francesco <francesco@DESKTOP-V9TD5M8>
2017-01-05 17:28:03 +0100

    tests added

--- src/lexicalpkg/ParserTester.java
+++ src/lexicalpkg/ParserTester.java
@@ -1,5 +1,11 @@
 package lexicalpkg;
 
-public class ParserTester {
-
+public class ParserTester 
+{
+	public static void main (String[] args) throws InvalidTokenException, UnexpectedTokenException
+	{
+		Lexer l = new Lexer("gol.txt");
+		Parser p = new Parser(l);
+		p.body();
+	}
 }
