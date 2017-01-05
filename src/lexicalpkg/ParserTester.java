e23c7e2604ca6fdc5b642dfc0e840d3226bbf1b5
francesco <francesco@DESKTOP-V9TD5M8>
2017-01-05 17:32:42 +0100

    old lexer in the default package was still there but i couldnt see it because default pckg was deleted (what kind of sorcery is this?). i created again the default pckg so i could delete old lexer.

--- src/lexicalpkg/ParserTester.java
+++ src/lexicalpkg/ParserTester.java
@@ -4,7 +4,7 @@ public class ParserTester
 {
 	public static void main (String[] args) throws InvalidTokenException, UnexpectedTokenException
 	{
-		Lexer l = new Lexer("gol.txt");
+		Lexer l = new Lexer("YonderWard/gol.txt");
 		Parser p = new Parser(l);
 		p.body();
 	}
