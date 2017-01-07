d71f19f2b5bae34eba87ec8271f0fd6ba0b957c1
OrangeNote <orangenote.mail@gmail.com>
2017-01-07 13:12:29 +0100

    added basic AST class

---
+++ src/lexicalpkg/AST.java
@@ -0,0 +1,23 @@
+package lexicalpkg;
+
+import java.util.List;
+
+import lexicalpkg.Lexer.Token;
+
+public class AST
+{
+	private Token token;
+	private List<AST> children;
+	
+	public AST(Token token, List<AST> children)
+	{
+		this.token = token;
+		this.children = children;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return String.format("AST(%s, %s)", token, children);
+	}
+}
